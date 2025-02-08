e23c01895f4c4b55c868b9f17c69b2f98e43e4e1
SahirAkbar <sahirakbar111@gmail.com>
2025-02-08 21:52:19 +0530

    entities added

--- src/main/java/com/hms/config/SecurityConfig.java
+++ src/main/java/com/hms/config/SecurityConfig.java
@@ -23,13 +23,13 @@ public class SecurityConfig {
 
 
         //haap
-//        http.authorizeHttpRequests().anyRequest().permitAll();
-        http.authorizeHttpRequests().
-                requestMatchers("/api/v1/users/login", "/api/v1/users/signUp","/api/v1/users/signup-propertyowner").
-                permitAll().
-                requestMatchers("/api/v1/users/get").hasRole("OWNEReyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWlrZSIsImV4cCI6MTczOTg5MjcxMCwiaXNzIjoic2FoaXRyIn0.WbdkyS6iyl-ynlXg_HiY57fc5sd0sf4MGwP2rdkwJ3k").
-                anyRequest().
-                authenticated();
+        http.authorizeHttpRequests().anyRequest().permitAll();
+//        http.authorizeHttpRequests().
+//                requestMatchers("/api/v1/users/login", "/api/v1/users/signUp","/api/v1/users/signup-propertyowner").
+//                permitAll().
+//                requestMatchers("/api/v1/users/get").hasRole("OWNEReyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWlrZSIsImV4cCI6MTczOTg5MjcxMCwiaXNzIjoic2FoaXRyIn0.WbdkyS6iyl-ynlXg_HiY57fc5sd0sf4MGwP2rdkwJ3k").
+//                anyRequest().
+//                authenticated();
          return http.build();
     }
 
--- src/test/java/com/hms/HmsApplicationTests.java
+++ src/test/java/com/hms/HmsApplicationTests.java
@@ -1,7 +1,10 @@
 package com.hms;
 
+import com.auth0.jwt.JWT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class HmsApplicationTests {
